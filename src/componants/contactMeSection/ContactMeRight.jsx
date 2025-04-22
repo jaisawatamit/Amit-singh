34b97450184ed664f7c9bde386c4352e532712d0
jaisawatamit <jaisawatamit@gmail.com>
2025-04-22 20:35:50 +0530

    image path correct

--- src/componants/contactMeSection/ContactMeRight.jsx
+++ src/componants/contactMeSection/ContactMeRight.jsx
@@ -6,7 +6,7 @@ function ContactMeRight() {
   return (
     <div className="flex flex-col items-center justify-center gap-12 text-white">
       <img
-        src="/public/public/images/email-image.png"
+        src="/images/email-image.png"
         alt="email image"
         className="max-w-[300px]"
       />
